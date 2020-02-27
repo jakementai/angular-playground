6f3635e8f7763956cd85ec6de301eaa944634d14
Jake <yeemh-wa16@student.tarc.edu.my>
2020-02-27 16:12:57 +0800

    Done until adding to cart

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,31 @@
+import { CartService } from './../cart.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  item;
+  constructor(
+    private route: ActivatedRoute,
+    private shoppingCart: CartService
+  ) { }
+
+  addToCart(product){
+    this.shoppingCart.addToCart(product);
+    window.alert('Item added to cart sucessfully!');
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(param => {
+      this.item = products[+param.get('productId')];
+    });
+  }
+
+
+}
